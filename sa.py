3e16ddb142990518dba2c67af4d6e4ca94079947
srinivas1820 <srinivasnbkr@gmail.com>
2021-10-27 17:42:33 +0530

    First

---
+++ sa.py
@@ -0,0 +1,4 @@
+num=int(input("enter a value:"))
+for i in range(1,num):
+        num=num*i
+        print(num)
---
+++ sr.py
@@ -0,0 +1,7 @@
+num=int(input("enter a value:"))
+if (num%2)== 0:
+    print("even")
+
+else:
+    print("odd")
+    
