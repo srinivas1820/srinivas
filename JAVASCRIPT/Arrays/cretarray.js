d51931102ce710ffea921a997dec98c806ea95cb
srinivas1820 <srinivasnbkr@gmail.com>
2021-11-16 18:35:11 +0530

    ok sr

---
+++ JAVASCRIPT/Arrays/cretarray.js
@@ -0,0 +1,13 @@
+ var a=[10,20,30,40,50]
+ console.log(a)
+ console.log(a[0])
+ console.log(a.length)
+//  console.log(a.append[70])
+ a.push(60)//ading first in element
+ console.log(a)
+ a.unshift(70)//reomeve the first element
+ console.log(a)
+ console.log(a.shift(0))
+ console.log(a)
+ delete a[0]
+ console.log(a)
--- JAVASCRIPT/looping/rev.js
+++ JAVASCRIPT/looping/rev.js
@@ -1,4 +1,9 @@
-for(i=10;i<=10;i-1)
-{
-    console.log(i)
+  var num=23456
+   var rem
+   rev=0
+while(num!=0){
+    rem=num%10
+    rev=rev*10+rem
+    num=num/10
+    console.log("rev")
 }
